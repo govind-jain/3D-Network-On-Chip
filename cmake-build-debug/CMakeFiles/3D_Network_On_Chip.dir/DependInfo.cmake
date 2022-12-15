9b05572ae85c7f5413c85498815ee2afb37226bb
yaswanth118 <yaswanthchunduru118@gmail.com>
2022-12-15 16:11:50 +0530

    Added Topology.txt file

---
+++ cmake-build-debug/CMakeFiles/3D_Network_On_Chip.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/chund/OneDrive/Desktop/3D-Network-On-Chip/src/noc/test/main.cpp" "CMakeFiles/3D_Network_On_Chip.dir/src/noc/test/main.cpp.o" "gcc" "CMakeFiles/3D_Network_On_Chip.dir/src/noc/test/main.cpp.o.d"
+  "/mnt/c/Users/chund/OneDrive/Desktop/3D-Network-On-Chip/src/noc/topology/IniReader.cpp" "CMakeFiles/3D_Network_On_Chip.dir/src/noc/topology/IniReader.cpp.o" "gcc" "CMakeFiles/3D_Network_On_Chip.dir/src/noc/topology/IniReader.cpp.o.d"
+  "/mnt/c/Users/chund/OneDrive/Desktop/3D-Network-On-Chip/src/noc/topology/TopologyConfig.cpp" "CMakeFiles/3D_Network_On_Chip.dir/src/noc/topology/TopologyConfig.cpp.o" "gcc" "CMakeFiles/3D_Network_On_Chip.dir/src/noc/topology/TopologyConfig.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/3D_Network_On_Chip.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "3D_Network_On_Chip"
+  "3D_Network_On_Chip.pdb"
+  "CMakeFiles/3D_Network_On_Chip.dir/src/noc/test/main.cpp.o"
+  "CMakeFiles/3D_Network_On_Chip.dir/src/noc/test/main.cpp.o.d"
+  "CMakeFiles/3D_Network_On_Chip.dir/src/noc/topology/IniReader.cpp.o"
+  "CMakeFiles/3D_Network_On_Chip.dir/src/noc/topology/IniReader.cpp.o.d"
+  "CMakeFiles/3D_Network_On_Chip.dir/src/noc/topology/TopologyConfig.cpp.o"
+  "CMakeFiles/3D_Network_On_Chip.dir/src/noc/topology/TopologyConfig.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3D_Network_On_Chip.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
