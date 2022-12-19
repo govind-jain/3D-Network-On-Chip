74d4ddb5569c4c4059b8a9648c3d41afdf4ac726
Govind Jain <hello@govindjain.com>
2022-12-19 15:47:43 +0530

    Standardized TopologyConfig

--- src/noc/topology/TopologyConfig.cpp
+++ src/noc/topology/TopologyConfig.cpp
@@ -1,57 +1,69 @@
-#include "TopologyConfig.h"
+#include "TopologyConfig.hpp"
 
-void f_Swap(int &u, int &v, vector<t_Coordinate > &p_Coordinates) {
-    if (get<0>(p_Coordinates[u]) != get<0>(p_Coordinates[v])) {
-        if (get<0>(p_Coordinates[u]) > get<0>(p_Coordinates[v])) {
-            swap(u, v);
+void f_Swap(t_SwitchId &p_SwitchId1, t_SwitchId &p_SwitchId2, vector<t_Coordinates> &p_CoordinatesList) {
+    if (get<0>(p_CoordinatesList[p_SwitchId1]) != get<0>(p_CoordinatesList[p_SwitchId2])) {
+        if (get<0>(p_CoordinatesList[p_SwitchId1]) > get<0>(p_CoordinatesList[p_SwitchId2])) {
+            swap(p_SwitchId1, p_SwitchId2);
         }
-    } else if (get<1>(p_Coordinates[u]) != get<1>(p_Coordinates[v])) {
-        if (get<1>(p_Coordinates[u]) > get<1>(p_Coordinates[v])) {
-            swap(u, v);
+    }
+    else if (get<1>(p_CoordinatesList[p_SwitchId1]) != get<1>(p_CoordinatesList[p_SwitchId2])) {
+        if (get<1>(p_CoordinatesList[p_SwitchId1]) > get<1>(p_CoordinatesList[p_SwitchId2])) {
+            swap(p_SwitchId1, p_SwitchId2);
         }
-    } else {
-        if (get<2>(p_Coordinates[u]) > get<2>(p_Coordinates[v])) {
-            swap(u, v);
+    }
+    else {
+        if (get<2>(p_CoordinatesList[p_SwitchId1]) > get<2>(p_CoordinatesList[p_SwitchId2])) {
+            swap(p_SwitchId1, p_SwitchId2);
         }
     }
 }
 
-pair<vector<t_Coordinate>, t_AdjacencyList> f_Reader() {
-    int n, m;
-    cin >> n >> m;
-    vector<t_Coordinate> l_Coordinates(n);
-    t_AdjacencyList l_AdjacencyList(n);
-    for (int i = 0; i < n; ++i) {
-        int x, y, z, p;
-        cin >> x >> y >> z >> p;
-        tuple<int, int, int> l_Coordinate = {x, y, z};
-        l_Coordinates[p] = l_Coordinate;
+tuple<int, vector<t_Coordinates>, t_AdjacencyList> f_Reader() {
+
+    int l_NumberOfNodes, l_NumberOfConnections;
+    cin >> l_NumberOfNodes >> l_NumberOfConnections;
+
+    vector<t_Coordinates> l_CoordinatesList(l_NumberOfNodes);
+    t_AdjacencyList l_AdjacencyList(l_NumberOfNodes);
+
+    for (int l_Counter = 0; l_Counter < l_NumberOfNodes; ++l_Counter) {
+        t_Coordinate l_xC, l_yC, l_zC;
+        t_SwitchId l_SwitchId;
+        cin >> l_xC >> l_yC >> l_zC >> l_SwitchId;
+        t_Coordinates l_Coordinates = {l_xC, l_yC, l_zC};
+        l_CoordinatesList[l_SwitchId] = l_Coordinates;
     }
-    for (int i = 0; i < m; ++i) {
-        int u, v, w;
-        cin >> u >> v >> w;
-        f_Swap(u,v,l_Coordinates);
-        l_AdjacencyList[u].push_back({v, w});
+
+    for (int l_Counter = 0; l_Counter < l_NumberOfConnections; ++l_Counter) {
+        t_SwitchId l_SwitchId1, l_SwitchId2;
+        int l_NumberOfIntermediateRepeaters;
+        cin >> l_SwitchId1 >> l_SwitchId2 >> l_NumberOfIntermediateRepeaters;
+        f_Swap(l_SwitchId1,l_SwitchId2,l_CoordinatesList);
+        l_AdjacencyList[l_SwitchId1].push_back({l_SwitchId2, l_NumberOfIntermediateRepeaters});
     }
-    return make_pair(l_Coordinates, l_AdjacencyList);
+
+    return make_tuple(l_NumberOfNodes, l_CoordinatesList, l_AdjacencyList);
 }
 
-void TopologyConfig::f_SetTopology() {
-    pair<vector<t_Coordinate>, t_AdjacencyList> p = f_Reader();
-    m_Position = p.first;
-    m_Neighbours = p.second;
-    m_NumNodes = m_Position.size();
-    cout << "(Debug)" << m_NumNodes << endl;
+void TopologyConfig::f_SetTopologyFromInput() {
+
+    tuple<int, vector<t_Coordinates>, t_AdjacencyList> l_InputReadings = f_Reader();
+
+    m_NumberOfNodes = get<0>(l_InputReadings);
+    m_CoordinatesList = get<1>(l_InputReadings);
+    m_AdjacencyList = get<2>(l_InputReadings);
+
+    cout << "(Debug)" << m_NumberOfNodes << endl;
 }
 
-t_AdjacencyList TopologyConfig::f_GetNeighbours() {
-    return m_Neighbours;
+t_AdjacencyList TopologyConfig::f_GetAdjacencyList() {
+    return m_AdjacencyList;
 }
 
-vector<t_Coordinate> TopologyConfig::f_GetPositions() {
-    return m_Position;
+vector<t_Coordinates> TopologyConfig::f_GetCoordinatesList() {
+    return m_CoordinatesList;
 }
 
-int TopologyConfig::f_GetNumNodes() {
-    return m_NumNodes;
+int TopologyConfig::f_GetNumberOfNodes() {
+    return m_NumberOfNodes;
 }
---
+++ src/noc/topology/TopologyConfig.hpp
@@ -0,0 +1,19 @@
+#ifndef INC_3D_NETWORK_ON_CHIP_TOPOLOGY_CONFIG_HPP
+#define INC_3D_NETWORK_ON_CHIP_TOPOLOGY_CONFIG_HPP
+
+#include "../types/types.hpp"
+#include <bits/stdc++.h>
+
+class TopologyConfig{
+private:
+    int m_NumberOfNodes;
+    vector<t_Coordinates> m_CoordinatesList;
+    t_AdjacencyList m_AdjacencyList;
+public:
+    void f_SetTopologyFromInput();
+    t_AdjacencyList f_GetAdjacencyList();
+    vector<t_Coordinates> f_GetCoordinatesList();
+    int f_GetNumberOfNodes();
+};
+
+#endif //INC_3D_NETWORK_ON_CHIP_TOPOLOGY_CONFIG_HPP
